bf46e89ee7c35b5f3ac6fdde720a66c169c1979c
AdemirBurgic <AdemirBurgic@users.noreply.github.com>
2019-10-02 16:40:56 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+asd
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err))
