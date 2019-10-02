d6dd44e394394b38b226e0637267c2bc49e8a431
AdemirBurgic <AdemirBurgic@users.noreply.github.com>
2019-10-02 17:02:03 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core'
+import { enableProdMode } from '@angular/core'asd
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
